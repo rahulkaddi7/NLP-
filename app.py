cf0b21b4b3dfe61069a8ec7429938f86c4923cac
Rahul Kaddi <iamrkaddi@gmail.com>
2025-08-21 02:44:37 +0530

    updating html

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 # Importing essential libraries
 from flask import Flask, render_template, request, redirect, url_for
 import pickle
@@ -6,7 +5,7 @@ import numpy as np
 import nltk
 
 
-# load pickle files 
+# load pickle files
 filename = 'nlp.pkl'
 classifier = pickle.load(open(filename, 'rb'))  ## classfier model
 cv = pickle.load(open('transform.pkl', 'rb'))   ## this is used for transforming the data using CV class
@@ -22,20 +21,24 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    errors=[]                                     ## for finding out errors
+    errors=[]
+    # Initialize message and emotion to handle cases where the form is empty
+    message = ""
+    emotion = ""
     if request.method == 'POST':
         try:                                      ## try block starts here
             message = request.form['text']        ## get the data from home.html
-            data = [message]                  
+            data = [message]
             vect = cv.transform(data).toarray()   ## transform them to array for prediction
             prediction = classifier.predict(vect) ## predict over the model
-            string = " "                          ## an empty string , will be used for getting the emotion  
+            string = " "
             emotion = string.join(prediction)     ## join to return the answer
         except:
             errors.append(
-                "Unable to get URL. Please make sure it's valid and try again."   ## If any errors would , then this block would execute
+                "Unable to process the request. Please try again."
             )
-    return render_template('home.html', prediction=emotion, errors=errors)        ## render the page for the user
+    # Pass the user's original text (message) back to the template
+    return render_template('home.html', prediction=emotion, errors=errors, user_text=message)
 
 
 if __name__ == '__main__':     ## Execution starts here
